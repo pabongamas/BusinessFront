42037fb62128aec47eb5e4a7dfe8b21fb3c9f3a4
pabongamas <pabongamas@gmail.com>
2023-10-26 18:11:42 -0500

    actualizacion de sidebar

--- src/app/website/modules/admin/components/layout/layout.component.ts
+++ src/app/website/modules/admin/components/layout/layout.component.ts
@@ -14,7 +14,10 @@ import {
   faUsersGear,
   faUsersLine,
   faRightFromBracket,
+  faAngleUp,
+  faAngleDown,
 } from '@fortawesome/free-solid-svg-icons';
+import { CdkAccordionModule } from '@angular/cdk/accordion';
 
 @Component({
   selector: 'app-layout',
@@ -27,17 +30,31 @@ import {
     RouterLinkActive,
     FontAwesomeModule,
     NgClass,
+    CdkAccordionModule,
   ],
 })
 export class LayoutComponent {
+  currentPath: string = '';
+  routesAdmin = ['/admin/rols', '/admin/users'];
   faBars = faBars;
   faUserGear = faUserGear;
   faChartLine = faChartLine;
   faUsersGear = faUsersGear;
   faUsersLine = faUsersLine;
+  faAngleUp = faAngleUp;
+  faAngleDown = faAngleDown;
   faRightFromBracket = faRightFromBracket;
   collapsedSidebar = false;
+  menuAdminOpen = false;
+  constructor(private router: Router) {
+    this.currentPath = this.router.url;
+    this.routesAdmin.includes(this.currentPath)
+      ? (this.menuAdminOpen = true)
+      : '';
+    // if(this.routesAdmin.includes(this.currentPath)){
 
+    // }
+  }
   toggleCollapsedBar() {
     console.log('aca');
     this.collapsedSidebar = !this.collapsedSidebar;
